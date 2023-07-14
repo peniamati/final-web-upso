43c575b048a309404fbfeb374e18b7083c5921d7
Matias Peña <pena_matias@hotmail.com>
2023-07-13 22:16:48 -0300

    Ahora si?

--- js/main.js
+++ js/main.js
@@ -439,19 +439,11 @@ function inicio() {
     var urlPagina = window.location.href;
     const selectedOption = selector.value;
     if (selectedOption !== "") {
-      if (urlPagina.indexOf("index.html") === -1){
         const searchParams = new URLSearchParams();
         searchParams.set("category", selectedOption);
         const newURL = urlPagina +"/index.html?" + searchParams.toString();
         window.location.href = newURL;
-      }
-      else{
-      const searchParams = new URLSearchParams();
-      searchParams.set("category", selectedOption);
-      const newURL = "/index.html?" + searchParams.toString();
-      window.location.href = newURL;
-    }
-    }
+    } 
   });
 
   fetch("https://b4you.free.mockoapp.net/categories")
