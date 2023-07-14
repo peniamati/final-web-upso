308d705a74401ecd368ed7f71b61ffad32fd1945
Matias Peña <pena_matias@hotmail.com>
2023-07-13 22:31:00 -0300

    Creo que se pudo

--- js/main.js
+++ js/main.js
@@ -440,10 +440,18 @@ function inicio() {
     const selectedOption = selector.value;
     
     if (selectedOption !== "") {
-      const searchParams = new URLSearchParams();
-      searchParams.set("category", selectedOption);
-      const newURL = urlPagina.replace(/\/index\.html.*$/, "/index.html") + "?" + searchParams.toString();
-      window.location.href = newURL;
+      if (urlPagina.indexOf("index.html") === -1){
+        const searchParams = new URLSearchParams();
+        searchParams.set("category", selectedOption);
+        const newURL = urlPagina +"/index.html?" + searchParams.toString();
+        window.location.href = newURL;
+      }
+      else{
+        const searchParams = new URLSearchParams();
+        searchParams.set("category", selectedOption);
+        const newURL = urlPagina.replace(/\/index\.html.*$/, "/index.html") + "?" + searchParams.toString();
+        window.location.href = newURL;
+      }
     }
   });
 
